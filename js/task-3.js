44d0a7543aa59e5d78fe725200a224295b73d94b
Yana Bondarenko <bondarenkoyana2406@gmail.com>
2025-11-30 21:51:23 +0100

    my homework 2

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,12 @@
-function getElementWidth(content, padding, border) {
-     const c = parseFloat(content);
-    const p = parseFloat(padding);
-    const b = parseFloat(border);
-     return c + p * 2 + b * 2;
-}
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+function checkForSpam(message) {
+     const normMessage = message.toLowerCase();
+    return normMessage.includes("spam") || normMessage.includes("sale");
+    }
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
