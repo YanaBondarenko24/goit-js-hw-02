fe7e73272a07622e6779b74d690df1bded988d60
Yana Bondarenko <bondarenkoyana2406@gmail.com>
2025-11-30 21:58:39 +0100

    my hw2 after fixed

--- js/task-4.js
+++ js/task-4.js
@@ -2,16 +2,19 @@ function getShippingCost(country) {
    let price;
     switch (country) {
         case `China`:
-            
+           price = 100; 
            return`Shipping to ${country} will cost ${price} credits`;
             
             case `Chile`:
+              price = 250;
             return`Shipping to ${country} will cost ${price}credits`;
            
             case `Australia`:
+                price = 170;
             return `Shipping to ${country} will cost ${price} credits`;
            
             case `Jamaica`:
+                price = 120;
           return`Shipping to ${country} will cost ${price} credits`;
            
         default:
